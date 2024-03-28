371a3aec6280683bb14be7908b5b5071b9ff5eb6
foramchhaya <foramkukadiya@gmail.com>
2024-03-28 12:56:38 -0400

    change default route

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/login", Controller.login_get);
 router.get("/dashboard", isValidUser, Controller.dashboard_get);
 router.post("/logout", Controller.logout_post);
-router.get("/home", Controller.home_get);
+router.get("/", Controller.home_get);
 router.get("/signup", Controller.signup_get);
 router.post("/signup", Controller.signup_post);
 router.post("/login", Controller.login_post);
